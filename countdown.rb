80e4854f944d8f99dae3e814cb6d451e6b204ed7
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 21:06:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(seconds)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(sec)
+  while sec > 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
+end
